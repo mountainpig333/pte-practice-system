6505705185ddfc2c695c23ac5b2f678c78c06e0e
mountainpig <mountainpig@mountainpigdeMac-mini.local>
2026-04-03 10:48:13 +0800

    fix: cookie sameSite=s lax for cross-origin

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use(session({
     secret: 'pte-practice-2026',
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 }
+    cookie: { maxAge: 24 * 60 * 60 * 1000, sameSite: 'lax', secure: false }
 }));
 
 function requireAuth(req, res, next) {
